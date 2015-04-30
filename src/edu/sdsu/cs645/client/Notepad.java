ff1a072c1787fccab1ed43dde79f02652e545f11
Pengyu Wang <bird0407@gmail.com>
2015-04-29 17:34:27 -0700

    add notepad panel

--- src/edu/sdsu/cs645/client/Notepad.java
+++ src/edu/sdsu/cs645/client/Notepad.java
@@ -16,19 +16,36 @@ public class Notepad implements EntryPoint {
 
     private final NotepadServiceAsync notepadService = GWT.create(NotepadService.class);
 
+    private FlowPanel headerPanel = new FlowPanel();
+    private FlowPanel loginPanel = new FlowPanel();
+    private Label errorLabel = new Label();
+    private PasswordTextBox passwordField = new PasswordTextBox();
+
+    private FlowPanel notepadPanel = new FlowPanel();
+    private FlowPanel buttonPanel = new FlowPanel();
+
     public void onModuleLoad() {
+        createHeader();
         createLoginPanel();
     }
 
+    private void createHeader() {
+        final HTML header = new HTML();
+        header.setHTML("<h1>Notepad</h1>");
+        headerPanel.add(header);
+
+        RootPanel.get().add(headerPanel);
+    }
+
     private void createLoginPanel() {
-        final FlowPanel loginPanel = new FlowPanel();
         final Button loginButton = new Button("Login");
-        final TextBox passwordField = new PasswordTextBox();
 
         loginPanel.addStyleName("loginPanel");
+        errorLabel.addStyleName("errorLabel");
 
         loginPanel.add(passwordField);
         loginPanel.add(loginButton);
+        loginPanel.add(errorLabel);
 
         RootPanel.get().add(loginPanel);
 
@@ -38,24 +55,46 @@ public class Notepad implements EntryPoint {
         passwordField.addKeyUpHandler(new KeyUpHandler() {
             public void onKeyUp(KeyUpEvent keyUpEvent) {
                 if (keyUpEvent.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    authenticateUser(passwordField.getText());
+                    authenticateUser();
                 }
             }
         });
 
         loginButton.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent clickEvent) {
-                authenticateUser(passwordField.getText());
+                authenticateUser();
             }
         });
     }
 
-    private void authenticateUser(String password) {
-        Label errorLabel = new Label();
+    private void authenticateUser() {
         errorLabel.setText("");
 
-        if (!FieldVerifier.isValidPassword(password)) {
+        if (!FieldVerifier.isValidPassword(passwordField.getText())) {
             errorLabel.setText("Invalid password. Please try again.");
+        } else {
+            removeLoginPanel();
+            createNotepadPanel();
         }
+    }
+
+    private void removeLoginPanel() {
+        RootPanel.get().remove(loginPanel);
+    }
+
+    private void createNotepadPanel() {
+        final Button saveButton = new Button("Save");
+        final Button loadButton = new Button("Load");
+        final RichTextArea editor = new RichTextArea();
+
+        notepadPanel.addStyleName("notepadPanel");
+        editor.addStyleName("editor");
+
+        buttonPanel.add(saveButton);
+        buttonPanel.add(loadButton);
+        notepadPanel.add(editor);
+
+        RootPanel.get().add(notepadPanel);
+        RootPanel.get().add(buttonPanel);
     }
 }
